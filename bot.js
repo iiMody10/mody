7857eae6609dd097e414c78666bca2cf6a304018
iiMody10 <44102988+iiMody10@users.noreply.github.com>
2018-10-13 23:27:27 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -465,16 +465,16 @@ client.on('message',function(message) {
        if(!muteRole) return message.guild.createRole({name: 'Muted'}).then(message.guild.channels.forEach(chan => chan.overwritePermissions(muteRole, {SEND_MESSAGES:false,ADD_REACTIONS:false})));
        if(!message.guild.member(message.author).hasPermission("MANAGE_ROLES")) return message.channel.send('ℹ **Error:** ``خصائص مفقودة``');
        if(!message.guild.member(client.user).hasPermission("MANAGE_ROLES")) return message.channel.send('ℹ **Error:** ``خصائص مفقودة مني``');
-       if(!muteMember) return message.channel.send('ℹ **Error:** ``منشن شخص``');
-       if(!muteReason) return message.channel.send('ℹ **Error:** ``حدد سباّ``');
-       if(!muteDuration) return message.channel.send('ℹ **Error:** ``حدد وقت زمني``');
+       if(!muteMember) return message.channel.send('ℹ **خطاء:** ``منشن الشخص الزى تريد اعطائة ميوت``');
+       if(!muteReason) return message.channel.send('ℹ **خطاء:** ``حدد سبب الميوت``');
+       if(!muteDuration) return message.channel.send('ℹ **خطاء:** ``حدد وقت زمني``');
        if(!muteDuration.match(/[1-7][s,m,h,d,w]/g)) return message.channel.send('ℹ **Error:** ``حدد وقت زمني صحيح``');
        message.channel.send(`:white_check_mark: **تم اعطاء العضو ميوت : ${muteMember}**`);
        muteMember.addRole(muteRole);
        muteMember.setMute(true)
        .then(() => { setTimeout(() => {
            muteMember.removeRole(muteRole)
-           muteMember.setMute(false)
+           muteMember.setMute(true)
        }, mmss(muteDuration));
        });
    } 
