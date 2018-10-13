258b1a0efd00c262da8d42f22af088c6a9b9fd7d
iiMody10 <44102988+iiMody10@users.noreply.github.com>
2018-10-13 02:42:02 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,36 +1,24 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const prefix = ''
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
+client.on('ready', async() => {
+var server = "489824023354671115"; // ايدي السررفر
+var channel = "493793930786111508";//ايدي الروم
+    setInterval(()=>{
+    client.guilds.get(server).channels.get(channel).send('**كودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودز **')
+    },305);
+})
 
-
-
-
-client.on('message', function(msg) {
-  if(msg.content.startsWith ('معلومات السيرفر')) {
-    if(!msg.channel.guild) return msg.reply('**❌ اسف لكن هذا الامر للسيرفرات فقط **');         
-    let embed = new Discord.RichEmbed()
-    .setColor('BLUE')
-    .setThumbnail(msg.guild.iconURL)
-    .addField('🌐 **اسم السيرفر : **' , `**[ __${msg.guild.name}__ ]**`,true)
-    .addField('🌍 ** موقع السيرفر :**',`**[ __${"EGYPT"}__ ]**`,true)
-    .addField('🎖** الرتب :**',`**[ __${msg.guild.roles.size}__ ]**`,true)
-    .addField('👤** عدد الاعضاء :**',`**[ __${msg.guild.memberCount}__ ]**`,true)
-    .addField('✅** عدد الاعضاء الاونلاين :**',`**[ __${msg.guild.members.filter(m=>m.presence.status == 'online').size}__ ]**`,true)
-    .addField('📝** الرومات الكتابية :**',`**[ __${msg.guild.channels.filter(m => m.type === 'text').size}__ ]**`,true)
-    .addField('🔊** رومات الصوت :**',`**[ __${msg.guild.channels.filter(m => m.type === 'voice').size}__ ]**`,true)
-    .addField('👑** صاحب السيرفر :**',`**[ __${msg.guild.owner}__ ]**`,true)
-    .addField('🆔** ايدي السيرفر :**',`**[ __${msg.guild.id}__ ]**`,true)
-    .addField('📅** تم عمل السيرفر في : **',msg.guild.createdAt.toLocaleString())
-    msg.channel.send({embed:embed});
-  }
-});
-
-
-
+client.on('ready', async() => {
+var server = "489824023354671115"; // ايدي السررفر
+var channel = "493793930786111508";//ايدي الروم
+    setInterval(()=>{
+    client.guilds.get(server).channels.get(channel).send('#daily')
+    },86400);
+})
 
 client.login(process.env.BOT_TOKEN);
