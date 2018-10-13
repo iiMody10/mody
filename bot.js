d34fa26afb4d69304c709fa6bfa919caeb698a74
iiMody10 <44102988+iiMody10@users.noreply.github.com>
2018-10-13 21:01:27 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -79,7 +79,7 @@ client.on('message',  message => {
 
 client.on('message', message => {
  
-    if(message.content === prefix + "f!mutechannel") {
+    if(message.content === prefix + "mutechannel") {
                         if(!message.channel.guild) return message.reply('** This command only for servers**');
  
 if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.reply(' **__ليس لديك صلاحيات__**');
@@ -91,7 +91,7 @@ if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.reply(' **__
            });
              }
 //FIRE BOT
- if(message.content === prefix + "f!unmutechannel") {
+ if(message.content === prefix + "unmutechannel") {
                      if(!message.channel.guild) return message.reply('** This command only for servers**');
  
 if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.reply('**__ليس لديك صلاحيات__**');
