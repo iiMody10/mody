f973c82181595b58a6fb5e528d5942861ccc9309
iiMody10 <44102988+iiMody10@users.noreply.github.com>
2018-10-13 20:56:37 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -65,4 +65,49 @@ client.on('message',  message => {
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+client.on('message', message => {
+ 
+    if(message.content === prefix + "f!mutechannel") {
+                        if(!message.channel.guild) return message.reply('** This command only for servers**');
+ 
+if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.reply(' **__ليس لديك صلاحيات__**');
+           message.channel.overwritePermissions(message.guild.id, {
+         SEND_MESSAGES: false
+ 
+           }).then(() => {
+               message.reply("**__تم تقفيل الشات__ :white_check_mark: **")
+           });
+             }
+//FIRE BOT
+ if(message.content === prefix + "f!unmutechannel") {
+                     if(!message.channel.guild) return message.reply('** This command only for servers**');
+ 
+if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.reply('**__ليس لديك صلاحيات__**');
+           message.channel.overwritePermissions(message.guild.id, {
+         SEND_MESSAGES: true
+ 
+           }).then(() => {
+               message.reply("**__تم فتح الشات__:white_check_mark:**")
+           });
+             }
+             
+     
+   
+});
+
+
+
+
 client.login(process.env.BOT_TOKEN);
