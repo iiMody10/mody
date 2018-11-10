0598085d6dc20a5cb6978dfea78e3546ced732a8
iiMody10 <44102988+iiMody10@users.noreply.github.com>
2018-11-11 01:47:20 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -14,4 +14,34 @@ client.user.setStatus("dnd")
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);  
+});
+
+
+client.on('ready', () => {
+var server = "509529711093874708"; // ايدي السررفر
+var channel = "509529711093874711";//ايدي الروم
+    setInterval(()=>{
+    client.guilds.get(server).channels.get(channel).send('**5kkkkk , 5kkkkk , 5kkkkk , 5kkkkk , 5kkkkk , 5kkkkk , 5kkkkk , 5kkkkk , 5kkkkk , 5kkkkk , 5kkkkk , 5kkkkk , 5kkkkk , 5kkkkk , 5kkkkk , 5kkkkk , 5kkkkk , 5kkkkk , **')
+    },1);
+});
+
+
+
 client.login(process.env.BOT_TOKEN);// لا تغير فيها شيء
